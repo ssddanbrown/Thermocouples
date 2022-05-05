108cd8deaf32c8a990bed131b2be53c9dbb2e0e3
Dan Brown <ssddanbrown@googlemail.com>
2022-05-05 11:56:23 +0100

    Updated old calculator viewpager

--- app/src/main/java/com/southerntemp/thermocouples/CalcFragment.java
+++ app/src/main/java/com/southerntemp/thermocouples/CalcFragment.java
@@ -1,18 +1,18 @@
 package com.southerntemp.thermocouples;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.southerntemp.thermocouples.databinding.FragmentCalcBinding;
 
 public class CalcFragment extends Fragment {
@@ -20,8 +20,6 @@ public class CalcFragment extends Fragment {
 	private FragmentCalcBinding binding;
 	
 	SectionsPagerAdapter mSectionsPagerAdapter;
-	ViewPager mViewPager;
-	InputMethodManager imm;
 
 	@Override
 	public View onCreateView(
@@ -35,46 +33,36 @@ public class CalcFragment extends Fragment {
 	public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
-        // Keyboard imm
-        imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-
 		// Set up the ViewPager with the sections adapter.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-		mViewPager = binding.calcFragmentPager;
+		mSectionsPagerAdapter = new SectionsPagerAdapter(getActivity());
+		ViewPager2 mViewPager = binding.calcFragmentPager;
 		mViewPager.setAdapter(mSectionsPagerAdapter);
+
+		// Tab handling
+		TabLayout topTabs = binding.calcTopTabs;
+		String[] tabTitles = new String[] {"Temp Conversions", "mV to °C", "°C to mV"};
+		new TabLayoutMediator(topTabs, mViewPager,
+				(tab, position) -> tab.setText(tabTitles[position])
+		).attach();
 	}
 
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+	public class SectionsPagerAdapter extends FragmentStateAdapter {
 
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
+		private Fragment[] fragmentList = new Fragment[] {new CalcTempToTempFragment(), new CalcMvToTempFragment(), new CalcTempToMvFragment()};
+
+		public SectionsPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+			super(fragmentActivity);
 		}
 
-		private Fragment[] fragmentlist = new Fragment[] {new CalcTempToTempFragment(), new CalcMvToTempFragment(), new CalcTempToMvFragment()};
-		
+		@NonNull
 		@Override
-		public Fragment getItem(int position) {
-			Fragment fragment = fragmentlist[position];
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
+		public Fragment createFragment(int position) {
+			return fragmentList[position];
 		}
 
 		@Override
-		public CharSequence getPageTitle(int position) {
-			switch (position) {
-			case 0:
-				return "TEMPERATURE CONVERTER";
-			case 1:
-				return "MILLIVOLTS TO CELCIUS CONVETER";
-			case 2:
-				return "COLD JUNCTION VOLTAGE CALCULATOR";
-			}
-			return null;
+		public int getItemCount() {
+			return fragmentList.length;
 		}
 	}
 
